8b189c3838c11a607946e1fd474597fb741322bd
Ivan <calintz1@hotmail.com>
2021-02-19 22:45:46 -0300

    update
    adicionado módulos

--- course-manager/src/app/app.module.ts
+++ course-manager/src/app/app.module.ts
@@ -1,14 +1,26 @@
-import { StarComponent } from './star/star.component';
-import { CourseListComponent } from './courses/course-list.component';
+import { CourseModule } from './courses/course.module';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
-
+import { RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
+import { CoreModule } from './core/core.module';
 
 @NgModule({
-  declarations: [AppComponent, CourseListComponent, StarComponent],
-  imports: [BrowserModule, FormsModule],
+  declarations: [AppComponent],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    CourseModule,
+    CoreModule,
+    RouterModule.forRoot([
+      {
+        path: '',
+        redirectTo: 'courses',
+        pathMatch: 'full',
+      },
+    ]),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
